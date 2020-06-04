0b07cfe8a5300359769d529fe6544a3baa7ee2a3
gbitan <guy.bitan@glassboxdigital.com>
2020-06-04 22:53:38 +0300

    Done

--- src/main/java/com/glassboxdigital/clients/ssh/TrafficGenerator.java
+++ src/main/java/com/glassboxdigital/clients/ssh/TrafficGenerator.java
@@ -1,12 +1,17 @@
 package com.glassboxdigital.clients.ssh;
 
+import com.glassboxdigital.SshCommands;
 import com.jcraft.jsch.Session;
+import org.apache.poi.ss.usermodel.Sheet;
 
-public class TrafficGenerator extends SshClient {
+public class TrafficGenerator extends SshClient implements SshCommands {
     public TrafficGenerator(String host, String user, String privateKeyLocation) {
         super(host, user, privateKeyLocation);
     }
 
+    public void createTopRow(Sheet sheet, int rowNumber) {
+        StringBuffer commands = runCommands(new String[]{PS_EF_STATUS});
+    }
 
     public boolean isUp() {
         String res = "";
