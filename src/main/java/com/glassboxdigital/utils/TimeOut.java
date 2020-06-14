ffd934fe3b1db2fd5d24fba59e25b1146f781cf2
gbitan <guy.bitan@glassboxdigital.com>
2020-06-14 16:49:52 +0300

    Phase 3 Done

--- src/main/java/com/glassboxdigital/utils/TimeOut.java
+++ src/main/java/com/glassboxdigital/utils/TimeOut.java
@@ -2,16 +2,16 @@ package com.glassboxdigital.utils;
 
 public class TimeOut {
 
-    long currentTime = 0;
+    long startTimeMs = 0;
     int timeoutMin = 0;
 
     public TimeOut(int timeOutMin) {
-        this.currentTime = System.currentTimeMillis();
+        this.startTimeMs = System.currentTimeMillis();
         this.timeoutMin = timeOutMin * 60 * 1000;
     }
 
     public boolean isTimeOutArrived() {
-        return (currentTime <= System.currentTimeMillis() + timeoutMin);
+        return (startTimeMs <= System.currentTimeMillis() + timeoutMin);
     }
 
 }
