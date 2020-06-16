b44c8e0a9f5abbe434fa05277e8086df9d5a3c5d
gbitan <guy.bitan@glassboxdigital.com>
2020-06-16 15:33:03 +0300

    Phase 5 Done

--- src/main/java/com/glassboxdigital/utils/TextFileLogger.java
+++ src/main/java/com/glassboxdigital/utils/TextFileLogger.java
@@ -33,7 +33,7 @@ public class TextFileLogger {
                        ext = "log";
                    }
                String filePath = System.getProperty("user.dir");
-               Path path = Paths.get(filePath + "/Error-logs");
+               Path path = Paths.get(filePath + "/Errors-logs");
         
                        try {
                        Files.createDirectory(path);
