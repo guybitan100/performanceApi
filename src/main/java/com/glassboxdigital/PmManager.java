aa88eb00405452e2e1f25800a07afe26efaf9060
gbitan <guy.bitan@glassboxdigital.com>
2020-07-01 10:32:07 +0300

    Phase 6 Done

--- src/main/java/com/glassboxdigital/PmManager.java
+++ src/main/java/com/glassboxdigital/PmManager.java
@@ -93,6 +93,7 @@ public class PmManager {
             }
             printAllErrors();
         }
+        //Send Mail
         if (!errorMessage.toString().isEmpty()) {
             Email.sendEmail(errorMessage.toString());
         }
